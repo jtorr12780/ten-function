002f9f96a0bb1a68be5fe30b1f7c74e4916fbf0e
Jose Torres <jose.alberto.torres35@gmail.com>
2022-06-06 15:21:59 -0500

    completed 10 function

--- solutions.js
+++ solutions.js
@@ -20,3 +20,56 @@
  * and
  * concat
  */
+function isTrue(input){
+    return input === true;
+}
+
+function isFalse(value){
+    return value === false;
+}
+
+function not(input){
+    return !input;
+}
+
+function addOne(input){
+    if (input == Infinity)
+        { return Infinity} else {
+             input= parseInt(input) + 1;
+             return input;
+    }
+
+}
+
+function isEven(number){
+    if (false)
+    {return false;}
+   else if(number % 2 === 0)
+    {return true;} else
+    {return false;}
+}
+
+function isIdentical(a, b){
+    if (a === b)
+        return true;
+        return false;
+}
+
+function isEqual(a, b){
+    if (a == b) {
+        return true;
+    }
+    else {   return false;
+    }
+}
+
+function or(a, b){
+    return a || b;
+}
+
+function and(a, b){
+    return a && b;
+}
+
+function concat(a, b){ return a.toString()+ b.toString()}
+
